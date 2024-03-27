f47a6d9e0358fc9004411bfc42d8dd979fe4ad66
Divnoor-Singh-Chugh <divnoor@gmail.com>
2024-03-27 13:54:20 +0530

    cookie error 2.0

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,6 +7,7 @@ const sendToken=(user,statusCode,res,message)=>{
             Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000
         ),
         secure: true,
+        SameSite:'None'
     }
     res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Secure; SameSite=None; Expires=${options.expires.toUTCString()}`);
     res.status(statusCode).cookie("token",token,options).json({
