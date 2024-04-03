615902803e6e25494896b5fe710aa40a44dd80f7
Divnoor-Singh-Chugh <divnoor@gmail.com>
2024-04-03 10:49:51 +0530

    allowHeaders

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const app=express();
 app.use(cors({
     origin:[process.env.FRONTEND_URL],
     methods:['GET','POST','DELETE','PUT'],
+    allowedHeaders:['Access-Control-Allow-Credentials'],
     credentials:true
 }))
 
