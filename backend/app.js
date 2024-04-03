d5ee29302528cf7244f2e3f9b1e5c3e1421d98ba
Divnoor-Singh-Chugh <divnoor@gmail.com>
2024-04-03 13:14:25 +0530

    Add more headers in cors

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,12 @@ app.use(cors({
     credentials:true
 }))
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://dreamy-sunshine-2eedbb.netlify.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
